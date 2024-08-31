535417e6ccc63702755da98347bcd3896a4166a5
Practicante CIE <practicante.cie@administrativos.local>
2024-08-31 10:27:18 -0500

    Diseño final del footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -13,30 +13,33 @@ function Footer() {
     <>
       <footer className="bg-custom-blue py-6 sm:py-8 md:py-10 text-white">
         <div className="container mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-8 relative">
             {/* CONTENIDO A LA IZQUIERDA */}
-            <div className="flex justify-center lg:justify-start items-center">
+            <div className="flex justify-center md:justify-start items-center">
               <img src={logoUCPblanco} alt="Logo UCP" className="h-16 sm:h-20 md:h-24 lg:h-28 w-auto" />
             </div>
 
             {/* CONTENIDO CENTRAL */}
-            <div className="flex flex-col space-y-4 items-center justify-center">
-              <div className="flex items-center">
+            <div className="flex flex-col space-y-4 items-start justify-center">
+              <div className="flex items-center w-full">
                 <img src={ubicacion} alt="Ubicación" className="h-5 sm:h-6 md:h-8 w-auto mr-2 flex-shrink-0" />
                 <p className="text-xs sm:text-sm">Carrera 21 No. 49-95 Av. de las Américas Pereira, Colombia.</p>
               </div>
-              <div className="flex items-center">
+              <div className="flex items-center w-full">
                 <img src={telefono} alt="Teléfono" className="h-4 sm:h-5 md:h-6 w-auto mr-2 flex-shrink-0" />
                 <p className="text-xs sm:text-sm">PBX (606) 3124000 EXT.1043</p>
               </div>
-              <div className="flex items-center">
+              <div className="flex items-center w-full">
                 <img src={correo} alt="Correo" className="h-4 sm:h-5 md:h-6 w-auto mr-2 flex-shrink-0" />
                 <p className="text-xs sm:text-sm">investigaciones@ucp.edu.co</p>
               </div>
             </div>
 
+            {/* LÍNEA VERTICAL */}
+            <div className="hidden md:block absolute right-1/3 top-[10%] h-[80%] w-px bg-white"></div>
+
             {/* CONTENIDO A LA DERECHA */}
-            <div className="flex flex-col items-center lg:items-end justify-center">
+            <div className="flex flex-col items-center md:items-end justify-center">
               <img src={ciencias} alt="Ciencias" className="h-10 sm:h-12 md:h-14 w-auto mb-4" />
               <div className="flex space-x-4">
                 <img src={youtube} alt="YouTube" className="h-6 sm:h-7 md:h-8 w-auto" />
@@ -59,4 +62,3 @@ function Footer() {
 }
 
 export default Footer
-
