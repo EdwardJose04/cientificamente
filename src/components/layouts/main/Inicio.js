9146e340072aef921cfbd931d6e6c7b6d670f936
Your Name <you@example.com>
2024-09-05 11:03:27 -0500

    Prueba

--- src/components/layouts/main/Inicio.js
+++ src/components/layouts/main/Inicio.js
@@ -138,7 +138,6 @@ function Inicio() {
           </div>
         </div>
       </div>
-
       {/* Portfolio Modal */}
       {showPortfolio && (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
