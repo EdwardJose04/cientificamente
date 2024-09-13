df74e0a30633271fc89dc06b13dfab61d46a00ce
Practicante CIE <practicante.cie@administrativos.local>
2024-09-13 17:03:28 -0500

    Genially añadidos a los proyectos

--- src/assets/json/proyectos.js
+++ src/assets/json/proyectos.js
@@ -25,7 +25,7 @@ const proyectos = {
           titulo: "CAPAZ",
           descripcion: "Centro analítico de producciones culturales",
           imagenes: [capaz],
-          genially: "capaz"
+          genially: "https://view.genially.com/66a3b0b1298c760bb31d63c3"
         },
         
       ]
@@ -39,7 +39,7 @@ const proyectos = {
           titulo: "IUMA",
           descripcion: "Laboratorio de co- creación simbólica artesanal para el empoderamiento y sostenibilidad socio-cultural de las mujeres indígenas Embera Chamí",
           imagenes: [iuma],
-          genially: "iuma"
+          genially: "https://view.genially.com/66a3c299e0c64b0109da4c30"
         },
       ]
     },
@@ -52,63 +52,63 @@ const proyectos = {
           titulo: "Competencias emocionales",
           descripcion: "Para favorecer la inclusión social entre pares en el marco de la multiculturalidad educativa",
           imagenes: [competencias],
-          genially: "competencias"
+          genially: "https://view.genially.com/66a7ed08a1e079dde88ad2c3"
         },
         {
           id: 2,
           titulo: "Adultez tardía",
           descripcion: "Programa de entrenamiento y estimulación de la memoria en el adulto mayor normal y patológico ",
           imagenes: [adultez],
-          genially: "adultez"
+          genially: "https://view.genially.com/66a50423a8ea2c4e38c75d55"
         },
         {
           id: 3,
           titulo: "Psicocalle",
           descripcion: "Características neuropsicológicas adolescentes infractores de las ciudades de Pereira, Bogotá y Neiva ",
           imagenes: [psicocalle],
-          genially: "psicocalle"
+          genially: "https://view.genially.com/66a51b77a8ea2c4e38d780d7"
         },
         {
           id: 4,
           titulo: "¡Echale gafa!",
           descripcion: "Estudio transcultutral sobre violencia de género y sus efectos en la salud mental de los jovenes ",
           imagenes: [echale_gafa],
-          genially: "echale_gafa"
+          genially: "https://view.genially.com/66a520a6c6b9b97645c8b377"
         },
         {
           id: 5,
           titulo: "Desarrollo socioemocional",
           descripcion: "De niños y adolescentes en Risaralda",
           imagenes: [socioemocional],
-          genially: "socioemocional"
+          genially: "https://view.genially.com/66a7b28aa1e079dde8647447"
         },
         {
           id: 6,
           titulo: "Vacunarte",
           descripcion: "Tecnoestrés y perspectivas de género: una lectura desde la salud mental ",
           imagenes: [vacunarte],
-          genially: "vacunarte"
+          genially: "https://view.genially.com/66a7a7eca1e079dde85cfc96"
         },
         {
           id: 7,
           titulo: "¡A mil por hora!",
           descripcion: "Fortalecimiento de competencias emocionales en adolescentes ",
           imagenes: [mil_por_hora],
-          genially: "mil_por_hora"
+          genially: "https://view.genially.com/66a7c4c0211e0130bc24296e"
         },
         {
           id: 8,
           titulo: "SMAF + ENFANCES",
           descripcion: "Salud mental - actividad física y bienestar: enfances 2/32",
           imagenes: [smaf],
-          genially: "smaf"
+          genially: "https://view.genially.com/66a7a0aa211e0130bc0b52bc"
         },
         {
           id: 9,
           titulo: "Entera-TEA",
           descripcion: "Red nacional de divulgación sobre el trastorno del espectro autista (TEA)",
           imagenes: [enteratea],
-          genially: "enteratea"
+          genially: "https://view.genially.com/66a7bc83a1e079dde86b7c2b"
         },
       ]
     },
@@ -121,21 +121,21 @@ const proyectos = {
           titulo: "CEMAB",
           descripcion: "Desarrollo de línea de envases para transporte y conservación de tejido vivo y estéril ",
           imagenes: [cemab],
-          genially: "cemab"
+          genially: "https://view.genially.com/66a3d646e0c64b0109e7d319"
         },
         {
           id: 2,
           titulo: "Nodo metalmecánico",
           descripcion: "Desarrollo y diseño de software para el fortalecimiento económico de Sector Metalmecánico risaraldense.  ",
           imagenes: [nodo_metal],
-          genially: "nodo_metal"
+          genially: "https://view.genially.com/66a406b3a8ea2c4e3852ab4f"
         },
         {
           id: 3,
           titulo: "Logística de medicamentos",
           descripcion: "Medicamentos oncológicos para las IPS de la ciudad de Pereira.",
           imagenes: [logistica],
-          genially: "logistica"
+          genially: "https://view.genially.com/66a401c4f82f380909a83e84"
         },
       ]
     },
@@ -148,7 +148,7 @@ const proyectos = {
           titulo: "Photoboy",
           descripcion: "Prevención de enfermedades ligadas al consumo de tabaco en niñas, niños y adolescentes del Eje Cafetero",
           imagenes: [photoboy],
-          genially: "photoboy"
+          genially: "https://view.genially.com/66a3cc06dc65a63cdfc1dc5a"
         }
       ]
     }
