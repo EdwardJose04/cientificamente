dc13e6bd925894391fde651625c3d38e8b79faa4
Practicante CIE <practicante.cie@administrativos.local>
2024-09-25 08:14:42 -0500

    Se alineo el texto a la izquierda junto a las imagenes en el componente de ABOUT

--- src/components/layouts/about/About.js
+++ src/components/layouts/about/About.js
@@ -8,41 +8,33 @@ import Maria from '../../../assets/img/about/maria.png'
 import Liliana from '../../../assets/img/about/liliana.png'
 
 function About() {
-
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
 
   return (
     <div className="min-h-screen">
-      {/* Componente Header */}
       <Header />
 
-      {/* Espacio en blanco */}
       <div className='h-10'></div>
 
-      {/* Sección Hero */}
       <div className="bg-custom-blue-2 py-16 px-4 sm:px-6 lg:px-8">
         <div className="max-w-7xl mx-auto flex flex-col md:flex-row items-center justify-between">
-          {/* Contenido textual */}
           <div className="md:w-1/2 mb-8 md:mb-0">
             <h2 className="text-4xl md:text-5xl font-semibold mb-4 text-white font-poppins">CIENTÍFICAMENTE</h2>
             <p className="text-xl text-white">Es una estrategia para promover el diálogo e intercambio de saberes entre la Universidad Católica de Pereira y las comunidades a través de la ciencia.</p>
           </div>
-          {/* Imagen ilustrativa */}
           <div className="md:w-1/2 flex justify-center">
             <img src={Biblioteca} alt="About Company" className="w-full max-w-md h-auto object-contain" />
           </div>
         </div>
       </div>
 
-      {/* Sección del Equipo */}
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16">
         <h3 className="text-3xl md:text-4xl lg:text-4xl font-semibold mb-12 text-left text-gray-800">
           Conoce a la <span className="text-custom-blue-2">Dirección de Investigaciones e Innovación</span>
         </h3>
 
-        {/* Grid de miembros del equipo */}
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
           {[
             { img: Cesar, name: "César Alberto Aristizabal", role: "Director" },
@@ -50,13 +42,11 @@ function About() {
             { img: Liliana, name: "Liliana Alejandra Cadena", role: "Profesional Administrativa Gestión de la Información" },
             { img: Karen, name: "Karen Nicolle López", role: "Auxiliar Administrativo" }
           ].map((person, index) => (
-            <div key={index} className="rounded-lg overflow-hidden transform transition duration-500 hover:scale-105">
-              {/* Imagen del miembro del equipo */}
-              <div className="h-64 overflow-hidden">
-                <img src={person.img} alt={person.name} className="w-full h-full object-cover" />
+            <div key={index} className="flex flex-col transform transition duration-500 hover:scale-105">
+              <div className="w-full overflow-hidden">
+                <img src={person.img} alt={person.name} className="w-full h-64 object-cover" />
               </div>
-              {/* Información del miembro del equipo */}
-              <div className="p-6">
+              <div className="mt-4">
                 <h3 className="text-xl font-semibold mb-2 text-gray-800">{person.name}</h3>
                 <p className="text-custom-blue-2 font-medium">{person.role}</p>
               </div>
@@ -65,7 +55,6 @@ function About() {
         </div>
       </div>
 
-      {/* Componente Footer */}
       <Footer />
     </div>
   )
